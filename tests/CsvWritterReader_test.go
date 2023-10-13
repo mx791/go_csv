f93cc5a2dfe04a0d2b8d9f519508f89ed57f3b6b
Charles <charles.laurioz@gmail.com>
2023-10-13 13:34:51 +0200

    maj tests

--- tests/CsvWritterReader_test.go
+++ tests/CsvWritterReader_test.go
@@ -10,6 +10,18 @@ func TestCreateCsvAndReadit(t *testing.T) {
 	df := DataFrame{make(map[string]Serie)}
 	df.setColumn("names", Serie{[]string{"A", "B", "C", "D"}})
 	df.setColumn("ages", Serie{[]string{"12", "15", "12", "7"}})
-	fmt.Println(df.size())
+	df.print(10)
+
+	if df.size() != 4 {
+		t.Fatalf("Size error")
+	}
+
+	df.toCsv("./out.csv")
+
+	df2 := DataFrameFromCsv("./out.csv")
+	df2.print(10)
+	if df2.size() != 4 {
+		t.Fatalf("Size error")
+	}
 
 }
