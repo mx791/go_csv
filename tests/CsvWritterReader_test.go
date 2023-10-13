50a593345809e19575ee4fc13f4208259c9b2d11
Charles <charles.laurioz@gmail.com>
2023-10-13 13:37:03 +0200

    maj tests

--- tests/CsvWritterReader_test.go
+++ tests/CsvWritterReader_test.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestCreateCsvAndReadit(t *testing.T) {
-
+func createDataframe() DataFrame {
 	df := DataFrame{make(map[string]Serie)}
 	df.setColumn("names", Serie{[]string{"A", "B", "C", "D"}})
 	df.setColumn("ages", Serie{[]string{"12", "15", "12", "7"}})
+	return df
+}
+
+func TestCreateCsvAndReadit(t *testing.T) {
+
+	df := createDataframe()
 	df.print(10)
 
 	if df.size() != 4 {
