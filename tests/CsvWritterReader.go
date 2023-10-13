bf3e419eb43c66d851ebbb1672c01dffd5c13dd7
Charles <charles.laurioz@gmail.com>
2023-10-13 13:30:34 +0200

    maj test

--- tests/CsvWritterReader.go
+++ tests/CsvWritterReader.go
@@ -1,9 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCreateCsvAndReadit(t *testing.T) {
 
 	df := DataFrame{make(map[string]Serie)}
+	df.setColumn("names", Serie{[]string{"A", "B", "C", "D"}})
+	df.setColumn("ages", Serie{[]string{"12", "15", "12", "7"}})
+	fmt.Println(df.size())
 
 }
