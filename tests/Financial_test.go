a9de4420d01b75642ee14dc16f42f6495b0b7c0f
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-02-22 16:14:43 +0100

    Update Financial_test.go

--- tests/Financial_test.go
+++ tests/Financial_test.go
@@ -3,12 +3,16 @@ package main
 import (
 	  "testing"
 	  "github.com/mx791/go_csv/dataframe"
+	  "fmt"
 )
 
 func TestSnP500(t *testing.T) {
 	dataframe.CSV_READER_SEPARTOR = ','
     	dataset := dataframe.DataFrameFromCsv("./test_data/sp500.csv")
+	
 	dataset.Print(15)
+	fmt.Println(dataset.Size())
+	
 	if dataset.Size() != 1769 {
 		t.Fatalf("Size error")
 	}
